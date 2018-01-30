ca3496511b09e97d86a227473b17ab6df101f88a
Brooke Lewis <belv2c@gmail.com>
2018-01-30 13:35:14 -0600

    finished money total exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -74,6 +74,35 @@ namespace LINQ
                 Console.WriteLine(n);
             }
 
+
+            // Aggregate Operations
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            int amountOfNumbers = numbers3.Count();
+            Console.WriteLine($"This list of numbers has {amountOfNumbers} numbers.");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double total = purchases.Sum();
+            Console.WriteLine($"We have made a total of ${total}.");
+            Console.ReadLine();
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            
         }
     }
 }
