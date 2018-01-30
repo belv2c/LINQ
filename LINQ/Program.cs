b5d45db52c31d404dd7139ddccfe35cbf4076590
Brooke Lewis <belv2c@gmail.com>
2018-01-29 21:04:00 -0600

    2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -28,7 +28,20 @@ namespace LINQ
             {
                 Console.WriteLine(item);
             }
+
+        // Which of the following numbers are multiples of 4 or 6
+        List<int> numbers = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+ 
+            var fourSixMultiples = numbers.Where(n => n % 2 == 0 || n % 6 == 0);
+
+            foreach (var n in fourSixMultiples)
+            {
+                Console.WriteLine(n);
+                Console.ReadLine();
+            }
         }
     }
 }
-  
