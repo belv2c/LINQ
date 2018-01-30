aac6239f8c8e2c0416bb8f1a5f36f51d6bf30493
Brooke Lewis <belv2c@gmail.com>
2018-01-29 20:40:11 -0600

    first exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,27 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            // Restriction/Filtering options
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>()
+            {
+                "Lemon",
+                "Apple",
+                "Orange",
+                "Lime",
+                "Watermelon",
+                "Loganberry"
+            };
+
+            var LFruits = from fruit in fruits
+                          where fruit.StartsWith("L")
+                          select fruit;
+
+            foreach (var item in LFruits)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
+  
